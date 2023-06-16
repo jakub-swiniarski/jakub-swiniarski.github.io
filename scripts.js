0e4bc04bfe5ad7df3c89d12cb54bf0fec7404205
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-06-16 16:25:05 +0200

    basic info

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ window.onload = function(){
     var currentYear = new Date().getFullYear();
     document.getElementById("copyright").innerHTML = "Copyright &copy; " + currentYear + " MrField. All rights reserved."
 };
+
+document.getElementById("t1").addEventListener("click", function() {
+    document.getElementById("info").innerHTML ="About me <br/> First name: Jacob <br/> Nationality: 🇵🇱 <br/> Hobbies: 🚲, 🏸";
+});
