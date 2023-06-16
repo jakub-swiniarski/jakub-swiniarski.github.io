df7e39ebd80b34889571ee1ac15f5b22d282b699
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-06-16 17:48:05 +0200

    renamed info header id

--- scripts.js
+++ scripts.js
@@ -4,21 +4,21 @@ window.onload = function(){
 };
 
 document.getElementById("t1").addEventListener("click", function() {
-    document.getElementById("inhd").innerHTML ="About me"
+    document.getElementById("info-header").innerHTML ="About me"
     document.getElementById("info").innerHTML ="First name: Jacob <br/> Nationality: 🇵🇱 <br/> Hobbies: 🚲, 🏸";
 });
 
 document.getElementById("t2").addEventListener("click", function() {
-    document.getElementById("inhd").innerHTML ="Education"
+    document.getElementById("info-header").innerHTML ="Education"
     document.getElementById("info").innerHTML ="edu";
 });
 
 document.getElementById("t3").addEventListener("click", function() {
-    document.getElementById("inhd").innerHTML ="Skills"
+    document.getElementById("info-header").innerHTML ="Skills"
     document.getElementById("info").innerHTML ="C++ <br/> Python <br/> Java <br/> HTML <br/> CSS <br/> JavaScript";
 });
 
 document.getElementById("t4").addEventListener("click", function() {
-    document.getElementById("inhd").innerHTML ="Quotes"
+    document.getElementById("info-header").innerHTML ="Quotes"
     document.getElementById("info").innerHTML ="quo";
 });
